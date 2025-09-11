d6085876c1e7b5288b946f7301d77602062f1bb0
Alex Yang <himself65@outlook.com>
2025-09-11 12:09:31 -0700

    fix: code

--- packages/better-auth/src/utils/get-request-ip.ts
+++ packages/better-auth/src/utils/get-request-ip.ts
@@ -13,7 +13,7 @@ export function getIp(
 		return "127.0.0.1"; // Use a fixed IP for test environments
 	}
 	if (isDevelopment) {
-		return testIP;
+		return "127.0.0.1"; // Use a fixed IP for development environments
 	}
 
 	const headers = "headers" in req ? req.headers : req;
