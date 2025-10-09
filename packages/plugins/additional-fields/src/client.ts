5f1b293add583523512cf6dcec320e86c6af8a34
Alex Yang <himself65@outlook.com>
2025-10-09 01:06:28 -0700

    refactor: additional fields plugin

---
+++ packages/plugins/additional-fields/src/client.ts
@@ -0,0 +1,78 @@
+import type { DBFieldAttribute } from "@better-auth/core/db";
+import type {
+	BetterAuthOptions,
+	BetterAuthClientPlugin,
+	BetterAuthPlugin,
+} from "@better-auth/core";
+
+export const inferAdditionalFields = <
+	T,
+	S extends {
+		user?: {
+			[key: string]: DBFieldAttribute;
+		};
+		session?: {
+			[key: string]: DBFieldAttribute;
+		};
+	} = {},
+>(
+	schema?: S,
+) => {
+	type Opts = T extends BetterAuthOptions
+		? T
+		: T extends {
+					options: BetterAuthOptions;
+				}
+			? T["options"]
+			: never;
+
+	type Plugin = Opts extends never
+		? S extends {
+				user?: {
+					[key: string]: DBFieldAttribute;
+				};
+				session?: {
+					[key: string]: DBFieldAttribute;
+				};
+			}
+			? {
+					id: "additional-fields-client";
+					schema: {
+						user: {
+							fields: S["user"] extends object ? S["user"] : {};
+						};
+						session: {
+							fields: S["session"] extends object ? S["session"] : {};
+						};
+					};
+				}
+			: never
+		: Opts extends BetterAuthOptions
+			? {
+					id: "additional-fields";
+					schema: {
+						user: {
+							fields: Opts["user"] extends {
+								additionalFields: infer U;
+							}
+								? U
+								: {};
+						};
+						session: {
+							fields: Opts["session"] extends {
+								additionalFields: infer U;
+							}
+								? U
+								: {};
+						};
+					};
+				}
+			: never;
+
+	return {
+		id: "additional-fields-client",
+		$InferServerPlugin: {} as Plugin extends BetterAuthPlugin
+			? Plugin
+			: undefined,
+	} satisfies BetterAuthClientPlugin;
+};
